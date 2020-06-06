b8eb0a64cb45704f7564bc578a5888aff6474d21
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-05 20:37:04 -0600

    Add first test for enigma class

---
+++ test/enigma_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/Enigma"
+
+class EnigmaTest < MiniTest::Test
+
+	def setup
+		@enigma = Enigma.new
+	end
+
+	def test_it_exists
+		assert_instance_of Enigma, @enigma
+	end
+
+end
