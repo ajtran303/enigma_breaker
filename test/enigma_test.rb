9999b70f7281a27f0a158ec2cbd56ea824a15024
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-05 20:48:32 -0600

    Add test for Enigma#no-crypt for prototype inputs

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,11 @@ class EnigmaTest < MiniTest::Test
 		assert_instance_of Enigma, @enigma
 	end
 
+  def test_it_can_pass_a_message_given_a_key_and_date
+    # use {encryption: "hello world"} for first tests
+    # change later to {encryption: "keder ohulw"}
+    expected_encryption = { encryption: "hello world", key: "02715", date: "040895" }
+    assert_equal expected_encryption, @enigma.no_crypt("hello world", "02715", "040895")
+  end
+
 end
