b41281bd4257e1d59d22edbe15bf8d9e15d7f10a
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-05 21:24:14 -0600

    Add test for Gear#make_keys

--- test/gear_test.rb
+++ test/gear_test.rb
@@ -11,4 +11,9 @@ class GearTest < MiniTest::Test
 		assert_instance_of Gear, @gear
 	end
 
+  def test_it_can_make_keys_from_a_seed
+    expected_keys = { A:"02", B:"27", C:"71", D:"15" }
+    assert_equal expected_keys, @gear.make_keys("02715")
+  end
+
 end
