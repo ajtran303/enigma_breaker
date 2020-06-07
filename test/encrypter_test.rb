fa4da5f78c3dd255473f94ed73dcd79f55d44b06
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 20:05:05 -0600

    Add test for Encrypter#translate helper method

--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -42,6 +42,7 @@ class EncrypterTest < MiniTest::Test
   end
 
   def test_it_will_encrypt_nothing_if_all_ciphers_added_are_zero_or_twenty_seven
+    skip
     tokens = [12, 24, 18, 15, 0, 2, 4, 26, 7, 0, 3, 26, "1", 12, 8, 11, 11, 8, 14, 13, 26, 20, 18, 4, 17, 18, 26, "@", 26, 19, 7, 4, 26, 1, 4, 6, "/", "2", "0", "0", "4", ".", 26, 22, 14, 22, "!"]
     expected = "myspace had 1million users @ the beg/2004. wow!"
 
@@ -62,5 +63,24 @@ class EncrypterTest < MiniTest::Test
     assert_equal expected, encrypter_3.encrypt(grouped_tokens)
   end
 
+  def test_it_can_translate
+    encrypter_1 = Encrypter.new
+    2.times { encrypter_1.add_cipher(0); encrypter_1.add_cipher(1) }
+    first_cipher, second_cipher, third_cipher, fourth_cipher = encrypter_1.ciphers
+
+    assert_equal "a", encrypter_1.translate(0, first_cipher)
+    assert_equal "b", encrypter_1.translate(1, second_cipher)
+    assert_equal "a", encrypter_1.translate(0, third_cipher)
+    assert_equal "b", encrypter_1.translate(1, fourth_cipher)
+
+    encrypter_2 = Encrypter.new
+    2.times { encrypter_2.add_cipher(0); encrypter_2.add_cipher(1) }
+    first_cipher, second_cipher, third_cipher, fourth_cipher = encrypter_2.ciphers
+
+    assert_equal ",", encrypter_2.translate(",", first_cipher)
+    assert_equal "\t", encrypter_2.translate("\t", second_cipher)
+    assert_equal "!", encrypter_2.translate("!", third_cipher)
+    assert_equal "@", encrypter_2.translate("@", fourth_cipher)
+  end
 
 end
