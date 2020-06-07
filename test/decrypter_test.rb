f9a30f48a8d15ad0f0a6170450fa0bf724fb61dd
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-07 00:20:40 -0600

    Add tests for Decrypter#add_cipher

--- test/decrypter_test.rb
+++ test/decrypter_test.rb
@@ -10,4 +10,28 @@ class DecrypterTest < MiniTest::Test
     assert_nil decrypter.terminal_tokens
   end
 
+  def test_it_can_add_a_cipher
+    decrypter = Decrypter.new
+    decrypter.add_cipher(0)
+
+    expected_sequence = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_includes decrypter.ciphers, expected_sequence
+  end
+
+  def test_it_can_add_many_ciphers
+    decrypter = Decrypter.new
+    decrypter.add_cipher(0)
+    decrypter.add_cipher(0)
+    decrypter.add_cipher(0)
+    decrypter.add_cipher(0)
+
+    assert_equal 4, decrypter.ciphers.count
+
+    expected_sequence = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal true, decrypter.ciphers.all? { |cipher| cipher.count == expected_sequence.count }
+    assert_equal 4, decrypter.ciphers.count { |cipher| cipher == expected_sequence }
+  end
+
+
 end
