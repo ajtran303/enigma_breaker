15cb8acfbdaa04d16106bcfe013a95d9a2b286be
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 19:14:36 -0600

    Add Encrypter attr_reader :ciphers

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -1,12 +1,14 @@
 class Encrypter
+  attr_reader :ciphers
+
+  def initialize
+    @ciphers = []
+  end
 
   def group_tokens(tokens)
     grouped_tokens = []
     tokens.each_slice(4) {|token_group| grouped_tokens << token_group}
     grouped_tokens
-  end
-
-  def cipher
   end
 
 end
