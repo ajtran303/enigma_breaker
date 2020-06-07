57c534c5740ae58ba111c63ef09dd2b50357b6dd
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 19:22:50 -0600

    Add Encrypter#add_cipher

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -1,3 +1,5 @@
+require './lib/rotator'
+
 class Encrypter
   attr_reader :ciphers
 
@@ -9,6 +11,10 @@ class Encrypter
     grouped_tokens = []
     tokens.each_slice(4) {|token_group| grouped_tokens << token_group}
     grouped_tokens
+  end
+
+  def add_cipher(rotations)
+    @ciphers << Rotator.get_sequence(rotations)
   end
 
 end
