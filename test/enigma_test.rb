3108fc943d876b60317be1ce3645354d3ac0f256
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-07 00:03:46 -0600

    Add test for Enigma#decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,11 +8,16 @@ class EnigmaTest < MiniTest::Test
 		assert_instance_of Enigma, enigma
 	end
 
-	def test_it_can_encrypt_a_message
+	def test_it_can_encrypt_a_message #with key and date
 		enigma = Enigma.new
-		#with key and date
 		expected = { encryption: "keder ohulw", key: "02715", date: "040895" }
 		assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+	end
+
+	def test_it_can_decrypt_a_message #with key and date
+		enigma = Enigma.new
+		expected = { decryption: "hello world", key: "02715", date: "040895" }
+		assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
 	end
 
 	def test_it_can_validate_input
