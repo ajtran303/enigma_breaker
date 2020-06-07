7ea57872769be382c0b94e9c418c459d7aea7328
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-07 00:17:30 -0600

    Add Decrypter class

---
+++ lib/decrypter.rb
@@ -0,0 +1,10 @@
+class Decrypter
+
+  attr_reader :ciphers, :terminal_tokens
+
+  def initialize
+    @ciphers = []
+    @terminal_tokens = nil
+  end
+  
+end
--- test/decrypter_test.rb
+++ test/decrypter_test.rb
@@ -6,6 +6,8 @@ class DecrypterTest < MiniTest::Test
   def test_it_exists_with_attributes
     decrypter = Decrypter.new
     assert_instance_of Decrypter, decrypter
+    assert_equal [], decrypter.ciphers
+    assert_nil decrypter.terminal_tokens
   end
 
 end
