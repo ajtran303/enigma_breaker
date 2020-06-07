e7a89afbf142884ba1b23eb4734f0c0b219ce4af
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 18:20:10 -0600

    Add test for Rotator#shift_sequence_by

--- test/rotator_test.rb
+++ test/rotator_test.rb
@@ -6,6 +6,16 @@ class RotatorTest < MiniTest::Test
   def test_it_exists_with_attributes
     rotator = Rotator.new
     assert_instance_of Rotator, rotator
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, rotator.character_sequence
+  end
+
+  def test_it_can_shift_sequence_by_a_number
+    rotator = Rotator.new
+    rotator.shift_sequence_by(1)
+    expected = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    assert_equal expected, rotator.character_sequence
   end
 
 end
