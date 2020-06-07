e560d3e0908f91b4231070c56cae4ce12950b4b8
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 18:30:12 -0600

    Add Rotator#shift_sequence_by

--- lib/rotator.rb
+++ lib/rotator.rb
@@ -1,2 +1,15 @@
 class Rotator
+
+  attr_reader :character_sequence
+
+  def initialize
+    @character_sequence = ("a".."z").to_a << " "
+  end
+
+  def shift_sequence_by(amount)
+    sequence = @character_sequence
+    amount = amount % sequence.size
+    @character_sequence = sequence.rotate(-amount)
+  end
+
 end
