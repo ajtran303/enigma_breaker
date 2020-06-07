2fea2f4eccc3ee6b34926a18427b7c92d81f7d93
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 19:00:04 -0600

    Add test for Encrypter#cipher

--- test/encrypter_test.rb
+++ test/encrypter_test.rb
@@ -17,4 +17,13 @@ class EncrypterTest < MiniTest::Test
     assert_equal expected, encrypter.group_tokens(tokens)
   end
 
+  def test_it_can_get_a_cipher_from_tokens
+    encrypter = Encrypter.new
+    tokens = [12, 24, 18, 15, 0, 2, 4, 26, 7, 0, 3, 26, "1", 12, 8, 11, 11, 8, 14, 13, 26, 20, 18, 4, 17, 18, 26, "@", 26, 19, 7, 4, 26, 1, 4, 6, "/", "2", "0", "0", "4", ".", 26, 22, 14, 22, "!"]
+
+    grouped_tokens = encrypter.group_tokens(tokens)
+    assert_equal "MySpace had 1Million users @ the beg/2004. Wow!", encrypter.cipher(grouped_tokens)
+  end
+
+
 end
