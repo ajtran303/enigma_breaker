caed4cb5dd3220569da87c1a7ef746a1bdea94cc
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 18:41:20 -0600

    Fix bug in setup for Rotator#shift_sequence_by

--- test/rotator_test.rb
+++ test/rotator_test.rb
@@ -14,7 +14,7 @@ class RotatorTest < MiniTest::Test
   def test_it_can_shift_sequence_by_a_number
     rotator_1 = Rotator.new
     rotator_1.shift_sequence_by(1)
-    expected = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    expected = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a"]
     assert_equal expected, rotator_1.character_sequence
 
     rotator_2 = Rotator.new
@@ -29,7 +29,7 @@ class RotatorTest < MiniTest::Test
 
     rotator_4 = Rotator.new
     rotator_4.shift_sequence_by(28)
-    expected = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    expected = ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a"]
     assert_equal expected, rotator_4.character_sequence
   end
 
