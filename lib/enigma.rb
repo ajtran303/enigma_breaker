7986b54bd2f57072d7beb4ee201603c39cf972a2
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-06 23:24:23 -0600

    Make Enigma#validate helper method to quit program when invalid input

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,12 +5,17 @@ require "./lib/encrypter"
 class Enigma
 
   def encrypt(*secret_message)
-    puts "That's not right!" unless valid?(secret_message); exit unless valid?(secret_message)
-    message, key, date = secret_message
-    shifts, key, date = Gear.get_shifts(key, date).values_at(:shifts, :key, :date)
-    tokens = Tokenizer.get_tokens(message)
+    validate(secret_message)
+    message_input, key_input, date_input = secret_message
+    shifts, key, date = Gear.get_shifts(key_input, date_input).values_at(:shifts, :key, :date)
+    tokens = Tokenizer.get_tokens(message_input)
     encryption = Encrypter.get_encryption(tokens, shifts)
     { encryption: encryption, key: key, date: date }
+  end
+
+  def validate(secret_message)
+    puts "That's not right!" unless valid?(secret_message)
+    exit unless valid?(secret_message)
   end
 
   def valid?(inputs)
