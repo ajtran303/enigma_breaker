2557d0bf7681e442b1c4b07798dcd4dccdcddd11
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-07 00:57:11 -0600

    Add tests for Decrypter#decrypt

--- test/decrypter_test.rb
+++ test/decrypter_test.rb
@@ -75,7 +75,6 @@ class DecrypterTest < MiniTest::Test
   end
 
   def test_it_can_translate
-    skip
     decrypter_1 = Decrypter.new
     2.times { decrypter_1.add_cipher(0); decrypter_1.add_cipher(1) }
     first_cipher, second_cipher, third_cipher, fourth_cipher = decrypter_1.ciphers
@@ -95,5 +94,51 @@ class DecrypterTest < MiniTest::Test
     assert_equal "@", decrypter_2.translate("@", fourth_cipher)
   end
 
+  def test_it_will_decrypt_nothing_if_all_ciphers_added_are_zero_or_twenty_seven
+    tokens = [12, 24, 18, 15, 0, 2, 4, 26, 7, 0, 3, 26, "1", 12, 8, 11, 11, 8, 14, 13, 26, 20, 18, 4, 17, 18, 26, "@", 26, 19, 7, 4, 26, 1, 4, 6, "/", "2", "0", "0", "4", ".", 26, 22, 14, 22, "!"]
+    expected = "myspace had 1million users @ the beg/2004. wow!"
+
+    decrypter_1 = Decrypter.new
+    4.times { decrypter_1.add_cipher(0) }
+    grouped_tokens = decrypter_1.group_tokens(tokens)
+    assert_equal expected, decrypter_1.decrypt(grouped_tokens)
+
+    decrypter_2 = Decrypter.new
+    4.times { decrypter_2.add_cipher(27) }
+    grouped_tokens = decrypter_2.group_tokens(tokens)
+    assert_equal expected, decrypter_2.decrypt(grouped_tokens)
+
+    decrypter_3 = Decrypter.new
+    2.times { decrypter_3.add_cipher(0) }
+    2.times { decrypter_3.add_cipher(27) }
+    grouped_tokens = decrypter_3.group_tokens(tokens)
+    assert_equal expected, decrypter_3.decrypt(grouped_tokens)
+
+    tokens = [12, 24, 18, 15, 0, 2, 4, 26]
+    expected = "myspace "
+    decrypter_4 = Decrypter.new
+    2.times { decrypter_4.add_cipher(0) }
+    2.times { decrypter_4.add_cipher(27) }
+    grouped_tokens = decrypter_4.group_tokens(tokens)
+    assert_equal expected, decrypter_4.decrypt(grouped_tokens)
+  end
+
+  def test_it_can_decrypt_which_means_encrypt_backwards
+    tokens = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3, "!"]
+    decrypter_1 = Decrypter.new
+    4.times { decrypter_1.add_cipher(0) }
+    secret_message = decrypter_1.group_tokens(tokens)
+    assert_equal "hello world!", decrypter_1.decrypt(secret_message)
+
+    decrypter_2 = Decrypter.new
+    4.times { decrypter_2.add_cipher(1) }
+    secret_message = decrypter_2.group_tokens(tokens)
+    assert_equal "gdkknzvnqkc!", decrypter_2.decrypt(secret_message)
+
+    decrypter_3 = Decrypter.new
+    2.times { decrypter_3.add_cipher(0); decrypter_3.add_cipher(1) }
+    secret_message = decrypter_3.group_tokens(tokens)
+    assert_equal "hdlkozwnrkd!", decrypter_3.decrypt(secret_message)
+  end
 
 end
