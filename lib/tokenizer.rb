cfd61fe616a4fbb6427e00bd911b7b624119fb19
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-07 19:51:56 -0600

    Use ternary operator to flatten Tokenizer#generate_tokens

--- lib/tokenizer.rb
+++ lib/tokenizer.rb
@@ -6,20 +6,18 @@ class Tokenizer
     @character_sequence = ("a".."z").to_a << " "
   end
 
-  def self.get_tokens(message)
-    tokenizer = Tokenizer.new
-    tokenizer.generate_tokens(message)
-  end
-
   def generate_tokens(message)
     characters = message.downcase.split("")
     characters.map do |character|
-      if character_sequence.include?(character)
-        character_sequence.index(character)
-      else
+      @character_sequence.include?(character) ?
+        @character_sequence.index(character) :
         character
-      end
     end
+  end
+
+  def self.get_tokens(message)
+    tokenizer = Tokenizer.new
+    tokenizer.generate_tokens(message)
   end
 
 end
