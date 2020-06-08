36124783f79a3278fcaf91fe8e7533c260035b03
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-06-08 01:39:43 -0600

    Make Sequencer super class for Rotator and Tokenizer

--- lib/rotator.rb
+++ lib/rotator.rb
@@ -1,12 +1,11 @@
 require "./lib/sequenceable"
+require "./lib/sequencer"
 
-class Rotator
+class Rotator < Sequencer
   include Sequenceable
 
-  attr_reader :character_sequence
-
   def initialize
-    @character_sequence = get_a_to_space_sequence
+    super
   end
 
   def shift_sequence_by(amount)
